5c20ca7368b17b26ecc87f8f1cc93dd20668994c
fengzhongzhu1621 <jinyinqiao@gmail.com>
2018-03-11 21:46:27 +0800

    [ADD]: add security

--- xTool/utils/net.py
+++ xTool/utils/net.py
@@ -23,6 +23,7 @@ def get_hostname():
 
     # Since we have a callable path, we try to import and run it next.
     # 根据配置文件中的设置的回调函数 module_path:attr_name 获取主机名
+    # 加载指定模块，获得模块定义的主机属性名
     module_path, attr_name = callable_path.split(':')
     module = importlib.import_module(module_path)
     callable = getattr(module, attr_name)
