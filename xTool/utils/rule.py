8818f08bc325e3dacb9b065563302726fa7ebdef
fengzhongzhu1621 <jinyinqiao@gmail.com>
2018-06-17 22:35:23 +0800

    [ADD]: add test_rule.;py

--- xTool/utils/rule.py
+++ xTool/utils/rule.py
@@ -6,16 +6,20 @@ from builtins import object
 
 
 class BaseRule(object):
+
+    _ALL_RULES = {}
+
     @classmethod
     def is_valid(cls, rule):
         return rule in cls.all_rules()
 
     @classmethod
     def all_rules(cls):
-        if not cls._ALL_TRIGGER_RULES:
-            cls._ALL_TRIGGER_RULES = {
+        if not cls._ALL_RULES:
+            # 获得属性值集合
+            cls._ALL_RULES = {
                 getattr(cls, attr)
                 for attr in dir(cls)
                 if not attr.startswith("_") and not callable(getattr(cls, attr))
             }
-        return cls._ALL_TRIGGER_RULES
+        return cls._ALL_RULES
