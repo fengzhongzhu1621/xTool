33e22765ed8bff8afd0138c6c7c1f9067f7029af
fengzhongzhu1621 <jinyinqiao@gmail.com>
2018-10-09 22:50:25 +0800

    [ADD]: add XToolBadRequest [ADD]: XToolNotFoundException

--- xTool/exceptions.py
+++ xTool/exceptions.py
@@ -2,7 +2,21 @@
 
 
 class XToolException(Exception):
-    pass
+    """
+    Base class for all Airflow's errors.
+    Each custom exception should be derived from this class
+    """
+    status_code = 500
+
+
+class XToolBadRequest(XToolException):
+    """Raise when the application or server cannot handle the request"""
+    status_code = 400
+
+
+class XToolNotFoundException(XToolException):
+    """Raise when the requested object/resource is not available in the system"""
+    status_code = 404
 
 
 class XToolConfigException(XToolException):
