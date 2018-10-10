f51b158ed3482db5b9c4ae4a49bb8cb9f2e5b0fe
fengzhongzhu1621 <jinyinqiao@gmail.com>
2018-10-10 23:38:55 +0800

    [IMP]: change comment [ADD]: add cryptography package

--- xTool/crypto/fernet.py
+++ xTool/crypto/fernet.py
@@ -2,13 +2,13 @@
 
 
 def generate_fernet_key():
-    """生成一个32位的随机数 ."""
+    """生成一个44字节的随机数 ."""
     try:
         from cryptography.fernet import Fernet
     except ImportError:
         pass
     try:
-        # 产生了一个32位随机数，并用base64编码，并解码为unicode编码
+        # 产生了一个44字节的随机数，并用base64编码，并解码为unicode编码
         key = Fernet.generate_key().decode()
     except NameError:
         key = "cryptography_not_found_storing_passwords_in_plain_text"
