cffb2e8fd91c5e9ab8b59a118513fa5078cf665d
fengzhongzhu1621 <jinyinqiao@gmail.com>
2018-10-16 23:40:42 +0800

    [ADD]: add processes.py

---
+++ tests/utils/test_processes.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+
+import os
+
+from xTool.utils import processes
+
+
+def test_which():
+    values = processes.which('Lib', path=None)
+    for executable_path in values:
+        assert os.access(executable_path, os.X_OK)
