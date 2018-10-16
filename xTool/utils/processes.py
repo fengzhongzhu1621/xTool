a9b30b2c83c46bce42bc756e3888fcd9a689b8f6
fengzhongzhu1621 <jinyinqiao@gmail.com>
2018-10-16 23:48:01 +0800

    [IMP]: add pwd, grp ID和名称转换关系

--- xTool/utils/processes.py
+++ xTool/utils/processes.py
@@ -2,12 +2,34 @@
 import os
 try:
     import pwd
+    import grp
 except ImportError:
     pass
 
 
+def uid_to_name( uid ):
+    """用户ID转换为用户名称 ."""
+    return pwd.getpwuid(uid).pw_name
+
+
+def gid_to_name(gid):
+    """组ID转换为组名 。"""
+    return grp.getgrgid( gid ).gr_name
+
+
+def name_to_uid( name ):
+    """用户名转换为用户ID ."""
+    return pwd.getpwnam( name ).pw_uid
+
+
+def name_to_gid( name ):
+    """组名转换为组ID ."""
+    return grp.getgrnam( name ).gr_gid
+
+
 def which(name, path=None):
     """
+    从指定路径或环境变量PATH中遍历所有的目录，判断文件名name是否在PATH目录下且可访问
     Look for an executable file of the given name in the given list of directories,
     or the directories listed in the PATH variable of the current environment. Roughly the
     equivalent of the `which` program. Does not work on Windows.
