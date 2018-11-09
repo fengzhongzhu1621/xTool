3b9cca42456f49fc152f3781957f6b1dd3675340
fengzhongzhu1621 <jinyinqiao@gmail.com>
2018-11-10 00:41:20 +0800

    [IMP]: add comment

--- xTool/jsonrpcserver/server.py
+++ xTool/jsonrpcserver/server.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 import logging
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
@@ -8,13 +9,19 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self) -> None:
         """HTTP POST"""
         # Process request
+        # 获得请求的body
         request = self.rfile.read(int(str(self.headers["Content-Length"]))).decode()
+        # 执行method
         response = dispatch(request)
         # Return response
+        # 如果不是通知，则返回结果
         if response.wanted:
+            # 设置返回状态码
             self.send_response(response.http_status)
+            # 设置json返回头
             self.send_header("Content-type", "application/json")
             self.end_headers()
+            # 返回响应
             self.wfile.write(str(response).encode())
 
 
