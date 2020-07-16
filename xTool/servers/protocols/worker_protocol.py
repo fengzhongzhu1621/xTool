90832dcf8b41b43b5025176921afcc8408812777
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-07-16 23:18:59 +0800

    feat: add protocol

---
+++ xTool/servers/protocols/worker_protocol.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+
+
+class WorkerProtocol(asyncio.Protocol):
+    __slots__ = ("transport", )
+
+    def __init__(self, *args, **kargs):
+        self.transport = None
+
+    def connection_made(self, transport):
+        """创建连接时执行 ."""
+        self.transport = transport
+
+    def data_received(self, data: bytes):
+        """接受到数据时调用 ."""
+        pass
+
+    def connection_lost(self, exc):
+        """连接丢失时执行 ."""
