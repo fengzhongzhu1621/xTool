0f563e1c2c484e2fe1d13c476789c03a27c4f97f
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-08-24 11:46:47 +0800

    feat: add weak.proxy

--- xTool/weak.py
+++ xTool/weak.py
@@ -5,10 +5,20 @@ from contextlib import suppress
 from math import ceil
 
 
+def proxy(obj):
+    """weakref.proxy的参数可以为proxy对象 ."""
+    if isinstance(obj, weakref.ProxyType):
+        return weakref.proxy(obj.__repr__.__self__)
+    else:
+        return weakref.proxy(obj)
+
+
 def _weakref_handle(info):  # type: ignore
     ref, name = info
+    # 如果弱引用的对象被回收，返回None
     ob = ref()
     if ob is not None:
+        # 忽略异常
         with suppress(Exception):
             getattr(ob, name)()
 
