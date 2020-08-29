7d22e1485d607517247136d00c37a4a7521f6282
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-08-29 20:42:57 +0800

    feat: add TCPServer

---
+++ tests/servers/test_tcp.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+import socket
+from xTool.servers.tcp import TCPServer
+
+
+def test_tcp_server(aiomisc_unused_port):
+    loop = asyncio.get_event_loop()
+
+    class TestTcpService(TCPServer):
+        DATA = []
+
+        async def handle_client(self, reader: asyncio.StreamReader,
+                                writer: asyncio.StreamWriter):
+            while True:
+                data = await reader.readline()
+                writer.write(data)
+                self.DATA.append(data)
+
+    service = TestTcpService(
+        "127.0.0.1", aiomisc_unused_port, **{"loop": loop})
+
+    async def writer():
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        with sock:
+            sock.connect(("127.0.0.1", aiomisc_unused_port))
+            sock.sendall(b"hello server\n")
+        await asyncio.sleep(1)
+
+    loop.run_until_complete(service.start())
+    loop.run_until_complete(writer())
+    loop.close()
+
+    assert TestTcpService.DATA == [b"hello server\n"]
---
+++ xTool/servers/tcp.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import asyncio
+import socket
+from functools import partial
+
+from xTool.aiomisc import awaitable
+from xTool.servers.helpers import bind_socket, OptionsType
+from xTool.servers.base import SimpleServer
+
+
+class TCPServer(SimpleServer):
+    PROTO_NAME = "tcp"
+
+    def __init__(
+        self, address: str = None, port: int = None,
+        options: OptionsType = (), sock=None, **kwargs
+    ):
+        if not sock:
+            if not (address and port):
+                raise RuntimeError(
+                    "You should pass socket instance or "
+                    '"address" and "port" couple',
+                )
+
+            self.make_socket = partial(
+                bind_socket,
+                address=address,
+                port=port,
+                options=options,
+            )
+        elif not isinstance(sock, socket.socket):
+            raise ValueError("sock must be socket instance")
+        else:
+            self.make_socket = lambda: sock
+
+        self.socket = None
+
+        super().__init__(**kwargs)
+
+    async def handle_client(
+        self, reader: asyncio.StreamReader,
+        writer: asyncio.StreamWriter
+    ):
+        raise NotImplementedError
+
+    def make_client_handler(
+        self, reader: asyncio.StreamReader,
+        writer: asyncio.StreamWriter,
+    ):
+        return self.create_task(awaitable(self.handle_client)(reader, writer))
+
+    async def start(self):
+        self.socket = self.make_socket()
+        self.server = await asyncio.start_server(
+            self.make_client_handler,
+            sock=self.socket,
+        )
+
+    async def stop(self, exc: Exception = None):
+        await super().stop(exc)
+        await self.server.wait_closed()
