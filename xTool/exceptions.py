1f414d5053a5cd5fec35b90bd3e6be771ac07861
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-11-16 23:34:10 +0800

    feat: 添加SDKError

--- xTool/exceptions.py
+++ xTool/exceptions.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-#
-# Note: Any AirflowException raised is expected to cause the TaskInstance
-#       to be marked in an ERROR state
+
+from enum import Enum
 
 from .status import STATUS_CODES
 
@@ -203,7 +185,8 @@ def abort(status_code, message=None):
         message = STATUS_CODES.get(status_code)
         # These are stored as bytes in the STATUS_CODES dict
         message = message.decode("utf8")
-    http_status_exception = _http_status_exceptions.get(status_code, HttpStatusException)
+    http_status_exception = _http_status_exceptions.get(
+        status_code, HttpStatusException)
     raise http_status_exception(message=message, status_code=status_code)
 
 
@@ -343,3 +326,17 @@ class XToolTimeoutError(AssertionError):
 
     def __str__(self):
         return repr(self.value)
+
+
+class ErrorCode(Enum):
+    # 成功
+    ErrCodeSuccess = 0
+
+
+class SDKError(Exception):
+    """sdk返回异常 ."""
+
+    def __init__(self, message: str, err_code: ErrorCode = None):
+        super().__init__(err_code, message)
+        self.err_code: ErrorCode = err_code
+        self.msg: str = message
