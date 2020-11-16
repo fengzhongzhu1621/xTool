f9b1973403b8934e3cfccbaa1079f2b337637390
fengzhongzhu1621 <jinyinqiao@gmail.com>
2020-11-16 23:34:53 +0800

    feat: 获得本机地址支持通过连接其他地址获取

--- xTool/misc.py
+++ xTool/misc.py
@@ -125,19 +125,30 @@ def getRunCommandResult(command):
     return (proc.returncode, stdoutdata, stderrdata)
 
 
-def get_local_host_ip(ifname=b'eth1'):
+def get_local_host_ip(ifname=b'eth1', ip: str = None, port: int = None):
     """获得本机的IP地址 ."""
     import platform
     import socket
+    if not ifname and ip and port:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect((ip, port))
+            return s.getsockname()[0]
+        finally:
+            s.close()
     if platform.system() == 'Linux':
         import fcntl
         import struct
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        o_ip = socket.inet_ntoa(fcntl.ioctl(
-            s.fileno(),
-            0x8915,
-            struct.pack('256s', ifname[:15])
-        )[20:24])
+        ifname = tob(ifname)
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            o_ip = socket.inet_ntoa(fcntl.ioctl(
+                s.fileno(),
+                0x8915,
+                struct.pack('256s', ifname[:15])
+            )[20:24])
+        finally:
+            s.close()
     else:
         o_ip = socket.gethostbyname(socket.gethostname())
     return o_ip
