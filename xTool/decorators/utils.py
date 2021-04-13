de9aff0ed4ace5160b0352913b956087bb2cd2ef
fengzhongzhu1621 <jinyinqiao@gmail.com>
2021-04-13 23:24:06 +0800

    feat: add safe_wraps()

--- xTool/decorators/utils.py
+++ xTool/decorators/utils.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from functools import wraps
+import functools
 
 
 class CallableContextManager(object):
     """将上下文管理器转换为装饰器 ."""
     def __call__(self, fn):
-        @wraps(fn)
+        @functools.wraps(fn)
         def inner(*args, **kwargs):
             with self:
                 return fn(*args, **kwargs)
         return inner
+
+
+def safe_wraps(wrapper, *args, **kwargs):
+    """Safely wraps partial functions."""
+    while isinstance(wrapper, functools.partial):
+        wrapper = wrapper.func
+    return functools.wraps(wrapper, *args, **kwargs)
