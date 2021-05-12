df1bcf77c3af96898bc2731a88d1620f475a901a
fengzhongzhu1621 <jinyinqiao@gmail.com>
2021-05-12 22:10:25 +0800

    feat: add PbJsonCodec

---
+++ tests/codec/test_pb_json_codec.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from xTool.codec.pb_json_codec import PbJsonCodec
+from .codec_pb2 import Request
+
+
+class TestPbJsonCodec:
+    def test_encode(self):
+        request = Request()
+        request.message = "hello world"
+        value_codec = PbJsonCodec.encode(request)
+        assert value_codec == '{"message": "hello world"}'
+
+        request2 = Request()
+        PbJsonCodec.decode(request2, value_codec)
+        assert request2.message == request.message
