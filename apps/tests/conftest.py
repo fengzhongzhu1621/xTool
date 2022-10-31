e1b38a88fc17ba1252691021f34dd5ba47ee5236
fengzhongzhu1621 <jinyinqiao@gmail.com>
2022-10-31 23:24:10 +0800

    test: add conftest.py

---
+++ apps/tests/conftest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.utils.functional import empty
+
+
+def pytest_configure():
+    # todo 添加需要修改的配置到 config_dict 中
+    config_dict = {}
+    if settings._wrapped is empty:
+        settings.configure(**config_dict)
