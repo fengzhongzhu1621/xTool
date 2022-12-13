307293e0c0f59cd9426e0486494abbb74cc26fb1
fengzhongzhu1621 <jinyinqiao@gmail.com>
2022-12-13 23:39:35 +0800

    feat: add CacheTypeItem

---
+++ apps/core/drf_resource/cache.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class CacheTypeItem:
+    """缓存类型 ."""
+
+    def __init__(self, key, timeout, user_related=None, label=""):
+        self.key = key
+        self.timeout = timeout
+        self.label = label
+        self.user_related = user_related
+
+    def __call__(self, timeout):
+        """类装饰器 ."""
+        return CacheTypeItem(self.key, timeout, self.user_related, self.label)
