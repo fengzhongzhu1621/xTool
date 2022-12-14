ff3cb160a896ab5111577ab90858ab6b8fcd61c4
fengzhongzhu1621 <jinyinqiao@gmail.com>
2022-12-14 22:30:37 +0800

    feat: add test_render.py

--- apps/tests/ notice/test_render.py
+++ apps/tests/ notice/test_render.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from apps.core.notice.constants import NoticeWay
-from apps.core.notice.render import NoticeRowRenderer
+from apps.core.notice.render import NoticeRowRenderer, CustomTemplateRenderer, CustomOperateTemplateRenderer
+from xTool.algorithms.collections import FancyDict
 
 
 class TestNoticeRowRenderer:
@@ -34,3 +35,43 @@ class TestNoticeRowRenderer:
         expect = """title_a: content_a
 title_b: content_b"""
         assert actual == expect
+
+
+class TestCustomTemplateRenderer:
+    def test_render(self):
+        content = "content"
+        context = {
+            "notice_way": NoticeWay.WECHAT,
+            "action": FancyDict({"id": 1}),
+            "a": "value_a",
+            "b": "value_b",
+            "title": "I'm title",
+            "title_template": "[{{action.id}}] {{title}}",
+            "default_title_template": "",
+            "content_template": """
+                #title_a#content_a {{a}}
+                #title_b#content_b {{b}}
+            """,
+            "default_content_template": "{{action.id}}",
+        }
+        CustomTemplateRenderer.render(content, context)
+        assert context["user_title"] == "[1] I'm title"
+        assert context["user_content"] == """title_a: content_a value_a
+title_b: content_b value_b"""
+
+
+class TestCustomOperateTemplateRenderer:
+    def test_render(self):
+        content = ""
+        context = {
+            "notice_way": NoticeWay.WECHAT,
+            "a": "value_a",
+            "b": "value_b",
+            "action_instance": FancyDict({"content_template": """
+                                    #title_a#content_a {{a}}
+                                    #title_b#content_b {{b}}
+                                """})
+        }
+        CustomOperateTemplateRenderer.render(content, context)
+        assert context["operate_content"] == """title_a: content_a value_a
+title_b: content_b value_b"""
